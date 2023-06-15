b8de47dd9c93e0c667921bba9ce7699b4f14346f
Chris Estreich <cestreich@gmail.com>
2023-06-15 10:20:48 -0700

    Add hooks package and husky + lint-staged pre-commit git hook

---
+++ packages/hooks/lib/use-window-event.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+/**
+ * useWindowEvent
+ */
+
+export function useWindowEvent<K extends keyof WindowEventMap>(
+  type: K,
+  listener: (this: Window, ev: WindowEventMap[K]) => void,
+  options?: boolean | AddEventListenerOptions,
+) {
+  useEffect(() => {
+    window.addEventListener(type, listener, options);
+    return () => window.removeEventListener(type, listener, options);
+  }, [type, listener, options]);
+}
