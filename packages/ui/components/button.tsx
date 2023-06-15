4285ea3480761495d8e50a05b42ac15e96640271
Chris Estreich <cestreich@gmail.com>
2023-06-15 10:06:57 -0700

    Move `cn` utility function

--- packages/ui/components/button.tsx
+++ packages/ui/components/button.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from '../lib/utils';
+import { cn } from './cn';
 
 const buttonVariants = cva(
   'relative inline-flex items-center justify-center rounded-md text-sm font-bold transition-colors ring-offset-background active:top-px focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
--- packages/ui/components/cn.ts
+++ packages/ui/components/cn.ts
@@ -1,4 +1,3 @@
-// 3rd party
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
